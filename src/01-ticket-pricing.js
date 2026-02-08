1e11a4cd2b81826952eaf9c1d15ef993afb8b877
NityaCode <code@nitya.page>
2026-02-08 20:04:54 +0530

    Solved: Ticket Pricing

--- src/01-ticket-pricing.js
+++ src/01-ticket-pricing.js
@@ -23,5 +23,25 @@
  * @returns {number} The ticket price, or -1 for invalid input
  */
 export function getTicketPrice(age, isWeekend) {
-  // Your code here
+  if (typeof age != 'number' || age < 0) {
+    return -1;
+  }
+
+  let ticketPrice = 0;
+
+  if (isWeekend) {
+    ticketPrice += 3;
+  }
+
+  if (age >= 0 && age <= 12) {
+    ticketPrice += 8;
+  } else if (age >= 13 && age <= 17) {
+    ticketPrice += 12;
+  } else if (age >= 18 && age <= 59) {
+    ticketPrice += 15;
+  } else if (age >= 60) {
+    ticketPrice += 10;
+  }
+
+  return ticketPrice;
 }
