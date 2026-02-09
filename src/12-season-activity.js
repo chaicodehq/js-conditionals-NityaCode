2f3fe0c3dcdc1c39a9a3ee14de6c208797bd2075
NityaCode <code@nitya.page>
2026-02-09 11:22:51 +0530

    Solution: Season Activity

--- src/12-season-activity.js
+++ src/12-season-activity.js
@@ -31,5 +31,53 @@
  * @returns {{ season: string, activity: string } | null}
  */
 export function getSeasonActivity(month, temperature) {
-  // Your code here
+  // Check for invalid month number
+  if (month < 1 || month > 12){
+    return null;
+  }
+
+  let activitySuggestion = {};
+
+  // Determine season & activity
+  if (month === 12 || month === 1 || month === 2){
+    
+    activitySuggestion["season"] = "Winter";
+
+    if (temperature < 0){
+      activitySuggestion["activity"] = "skiing";
+    } else if (temperature >= 0){
+      activitySuggestion["activity"] = "ice skating";
+    }
+  } else if (month === 3 || month === 4 || month === 5){
+    
+    activitySuggestion["season"] = "Spring";
+
+    if (temperature > 20){
+      activitySuggestion["activity"] = "hiking";
+    } else if (temperature <= 20){
+      activitySuggestion["activity"] = "museum visit";
+    }
+  }
+  if (month === 6 || month === 7 || month === 8){
+    
+    activitySuggestion["season"] = "Summer";
+
+    if (temperature > 35){
+      activitySuggestion["activity"] = "swimming";
+    } else if (temperature <= 35){
+      activitySuggestion["activity"] = "cycling";
+    }
+  }
+  if (month === 9 || month === 10 || month === 11){
+    
+    activitySuggestion["season"] = "Autumn";
+
+    if (temperature > 15){
+      activitySuggestion["activity"] = "nature walk";
+    } else if (temperature <= 15){
+      activitySuggestion["activity"] = "reading at a cafe";
+    }
+  }
+
+  return activitySuggestion;
 }
