528cf7723bba2b2d002f5f14602806df1aa63bb6
NityaCode <code@nitya.page>
2026-02-09 08:06:53 +0530

    Solution: Tax Calculator

--- src/08-tax-calculator.js
+++ src/08-tax-calculator.js
@@ -26,5 +26,37 @@
  * @returns {number} Total tax amount owed
  */
 export function calculateTax(income) {
-  // Your code here
+  // Check for no/negative income
+  if (income <= 0){
+    return 0;
+  }
+
+  let amountToTax = income;
+  let totalTax = 0;
+
+  // Zero tax for income up to $10,000
+  if (amountToTax > 70000){
+    totalTax += ((amountToTax - 70000) * 30) / 100;
+    amountToTax = 70000;
+  }
+
+  // Tax for 30,001 - 70,000 slab
+  if (amountToTax > 30000){
+    totalTax += ((amountToTax - 30000) * 20) / 100;
+    amountToTax = 30000;
+  }
+
+  // Tax for 10,001 - 30,000 slab
+  if (amountToTax > 10000){
+    totalTax += ((amountToTax - 10000) * 10) / 100;
+    amountToTax = 10000;
+  }
+
+  // Tax for 1 - 10,000 slab
+  if (amountToTax > 10000){
+    totalTax += 0;
+    amountToTax = 0;
+  }
+
+  return totalTax;
 }
